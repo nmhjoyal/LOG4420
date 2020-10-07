7eebc2158dd5d658fe178b1ec33c7a0dd7dcfff5
nmhjo <nicole.joyal@polymtl.ca>
2020-10-07 18:01:25 -0400

    Page pour un produit générée dynamiquement

--- TP3/assets/scripts/main.js
+++ TP3/assets/scripts/main.js
@@ -1,48 +1,46 @@
-sessionStorage.setItem("shoppingCartItems", JSON.stringify(["", "allo"]));
+// Header
+updateShoppingCartView();
 
-const listeCount = document.getElementsByClassName("count");
-
-Array.from(listeCount).forEach(element => {
-    if (JSON.parse(sessionStorage.getItem("shoppingCartItems")).length === 0) {
-        element.style.visibility = "hidden";
+function updateShoppingCartView() {
+    if (sessionStorage.getItem("shoppingCartItems") === null) {
+        sessionStorage.setItem("shoppingCartItems", JSON.stringify([]));
+    }
+    const shoppingCartList = JSON.parse(sessionStorage.getItem("shoppingCartItems"));
+    if (shoppingCartList.length === 0) {
+        $(".count").attr("style", "visibility: hidden;");
     } else {
-        element.style.visibility = "visible";
-        element.innerHTML = JSON.parse(sessionStorage.getItem("shoppingCartItems")).length;
+        $(".count").attr("style", "visibility: visible;");
+        $(".count").html(shoppingCartList.length);
     }
+}
+
+// Products
+let products = {category: "all", sort: "lowestHighest", productList: []};
+let fullProductList = [];
+
+$.get("/data/products.json", function(data) {
+    fullProductList = data;
+    products = {category: "all", sort: "lowestHighest", productList: fullProductList.sort((a, b) => { return a.price - b.price; })};
+    recreateProductList();
+    populateProductPage();
 });
-
-
-var request = new XMLHttpRequest();
-request.open("GET", "http://localhost:8000/data/products.json");
-var products = {category: "all", sort: "lowestHighest", productList: []};
-var fullProductList = [];
-
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        const response = JSON.parse(this.responseText);
-        fullProductList = response;
-        products = {category: "all", sort: "lowestHighest", productList: response.sort((a, b) => { return a.price - b.price; })};
-        recreateProductList();
-    }
-};
-request.send();
 
 function filterView(category) {
     products.category = category;
     sortAndFilterProducts();
-    Array.from(document.getElementById("product-categories").children).forEach((child) => {
+    Array.from($("#product-categories").children()).forEach((child) => {
         child.removeAttribute("class");
     });
-    document.getElementById(category).setAttribute("class", "selected");
+    $("#".concat(category)).addClass("selected");
 }
 
 function sortView(sortType) {
     products.sort = sortType;
     sortAndFilterProducts();
-    Array.from(document.getElementById("product-criteria").children).forEach((child) => {
+    Array.from($("#product-criteria").children()).forEach((child) => {
         child.removeAttribute("class");
     });
-    document.getElementById(sortType).setAttribute("class", "selected");
+    $("#".concat(sortType)).addClass("selected");
 
 }
 
@@ -74,32 +72,93 @@ function sortAndFilterProducts() {
             else return 0;
         });
     }
-    document.getElementById("products-count").innerHTML = products.productList.length.toString().concat(" produits");
+    $("#products-count").html(products.productList.length.toString().concat(" produits"));
     recreateProductList();
 }
 
 function recreateProductList() {
-    const productListDiv = document.getElementById("products-list");
-    productListDiv.innerHTML = "";
-    products.productList.forEach(product => {
-        const newProduct = document.createElement("div");
-        newProduct.setAttribute("class", "product");
-        const productLink = document.createElement("a");
-        productLink.setAttribute("href", "./product.html?id=".concat(product.id));
-        productLink.setAttribute("title", "En savoir plus...");
-        const productHeader = document.createElement("h2");
-        productHeader.innerHTML = product.name;
-        const productImg = document.createElement("img");
-        productImg.setAttribute("alt", product.name);
-        productImg.setAttribute("src", "./assets/img/".concat(product.image));
-        const productPrice = document.createElement("p");
-        productPrice.setAttribute("class", "price");
-        productPrice.innerHTML = "<small>Prix</small> ".concat(product.price).concat("&thinsp;$");
+    const productListDiv = $("#products-list");
+    if (productListDiv != undefined) {
+        productListDiv.html("");
+        products.productList.forEach(product => {
+            const newProduct = document.createElement("div");
+            newProduct.setAttribute("class", "product");
+            const productLink = document.createElement("a");
+            productLink.setAttribute("href", "./product.html?id=".concat(product.id));
+            productLink.setAttribute("title", "En savoir plus...");
+            const productHeader = document.createElement("h2");
+            productHeader.innerHTML = product.name;
+            const productImg = document.createElement("img");
+            productImg.setAttribute("alt", product.name);
+            productImg.setAttribute("src", "./assets/img/".concat(product.image));
+            const productPrice = document.createElement("p");
+            productPrice.setAttribute("class", "price");
+            productPrice.innerHTML = "<small>Prix</small> ".concat(product.price).concat("&thinsp;$");
 
-        productLink.appendChild(productHeader);
-        productLink.appendChild(productImg);
-        productLink.appendChild(productPrice);
-        newProduct.appendChild(productLink);
-        productListDiv.appendChild(newProduct);
+            productLink.appendChild(productHeader);
+            productLink.appendChild(productImg);
+            productLink.appendChild(productPrice);
+            newProduct.appendChild(productLink);
+            productListDiv.append(newProduct);
+        });
+    }
+}
+
+// Product
+$.urlParam = function(name) {
+    const results = new RegExp("[?&]".concat(name).concat("=([^&#]*)")).exec(window.location.href);
+    if (results === null) {
+        return null;
+    } else {
+        return results[1] || 0;
+    }
+};
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+const productId = $.urlParam("id");
+
+function populateProductPage() {
+    if (productId != undefined) {
+        const selectedProduct = fullProductList.find((product) => { return product.id == productId; });
+        if (selectedProduct != undefined) {
+            $("#product-image").attr("alt", selectedProduct.name);
+            $("#product-image").attr("src", "./assets/img/".concat(selectedProduct.image));
+            $("#product-name").html(selectedProduct.name);
+            $("#product-desc").html(selectedProduct.description);
+            $("#product-price").html("Prix: <strong>".concat(selectedProduct.price).concat("&thinsp;$</strong>"));
+            const featureList = $("#product-features");
+            Array.from(selectedProduct.features).forEach((feature) => {
+                const newFeature = document.createElement("li");
+                newFeature.innerHTML = feature;
+                featureList.append(newFeature);
+            });
+        } else {
+            $("main").html("");
+            const errorText = document.createElement("h1");
+            errorText.innerHTML = "Page non trouvée!";
+            $("main").append(errorText);
+        }
+    }
+}
+
+const addToCartForm = document.getElementById("add-to-cart-form");
+if (addToCartForm != undefined) {
+    addToCartForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const quantity = $("#product-quantity").val();
+        let cartList = JSON.parse(sessionStorage.getItem("shoppingCartItems"));
+        const selectedProduct = fullProductList.find((product) => { return product.id == productId; });
+        if (selectedProduct != undefined) {
+            for (let i = 0; i < quantity; i++) {
+                cartList.push(selectedProduct);
+            }
+        }
+        sessionStorage.setItem("shoppingCartItems", JSON.stringify(cartList));
+        dialog.innerHTML = "Le produit a été ajouté au panier.";
+        updateShoppingCartView();
+        $("#dialog").show();
+        setTimeout(function() {
+            $("#dialog").hide();
+        }, 5000);
     });
 }
